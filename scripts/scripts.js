e48b0d76fa09841a15cd065bfb5f6404ee8b65d2
Lesha Popovkin <leshapopovkin@yandex.ru>
2023-05-06 21:20:12 +0400

    feat: js code + mobile version of popup

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,18 +1,34 @@
+// данные со странички
 const profile = document.querySelector('.profile');
 const profileName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__description');
-const editProfile = document.querySelector('.profile__button_type_edit');
-
-const popup = document.querySelector('.popup')
-const closePopup = document.querySelector('.popup__button_type_close');
-
-function popupOpen() {
+// кнопка на страничке
+const profileEdit = document.querySelector('.profile__button_type_edit');
+// попап
+const popup = document.querySelector('.popup');
+const popupBlock = document.querySelector('.popup__block');
+// инпуты
+const nameInput = document.querySelector('.popup__input[name="name"]')
+const descriptionInput = document.querySelector('.popup__input[name="description"]')
+// кнопка в попапе
+const popupClose = document.querySelector('.popup__button_type_close');
+// открыть попап
+function openPopup() {
     popup.classList.add("popup_opened");
+    nameInput.value = profileName.textContent;
+    descriptionInput.value = profileDescription.textContent;
 }
-
-function popupClose() {
+//закрыть попап
+function closePopup() {
     popup.classList.remove("popup_opened");
 }
-
-editProfile.addEventListener('click', popupOpen);
-closePopup.addEventListener('click', popupClose);
+// сохранить данные в попапе
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    (profileName.textContent = nameInput.value) && (profileDescription.textContent = descriptionInput.value);
+    closePopup();
+}
+// вызов функций
+profileEdit.addEventListener('click', openPopup);
+popupClose.addEventListener('click', closePopup);
+popupBlock.addEventListener('submit', formSubmitHandler);
