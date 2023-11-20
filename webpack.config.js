90610e6ba2178fa988fe0b8444a79f377e92cc4b
Lesha Popovkin <leshapopovkin@yandex.ru>
2023-11-20 21:40:55 +0400

    trying...

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 module.exports = {
     entry: {
-        main: './src/pages/index.js'
+        main: '/src/pages/index.js'
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
